1f79c39b7677b267c7e8734a144bab720010ce67
yurasytnyk <yurii.sytnyk@spd-ukraine.com>
2026-01-14 13:17:21 +0200

    chore: update import paths

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,5 @@
 import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui";
+import { Button } from "@yurasytnyk/ui";
 import styles from "./page.module.css";
 
 type Props = Omit<ImageProps, "src"> & {
